2bfa1926327389262c8430a7fcc177ebab770806
Wez Furlong <wez@wezfurlong.org>
2018-09-15 09:12:50 -0700

    Update documentation

---
+++ atsamd21e18a/aliases.js
@@ -0,0 +1,17 @@
+var ALIASES = {};
+ALIASES['aligned'] = {};
+ALIASES['atsamd21_hal'] = {};
+ALIASES['atsamd21e18a'] = {};
+ALIASES['bare_metal'] = {};
+ALIASES['bitfield'] = {};
+ALIASES['cortex_m'] = {};
+ALIASES['cortex_m_rt'] = {};
+ALIASES['embedded_hal'] = {};
+ALIASES['gemma_m0'] = {};
+ALIASES['mashup'] = {};
+ALIASES['nb'] = {};
+ALIASES['proc_macro_hack'] = {};
+ALIASES['r0'] = {};
+ALIASES['vcell'] = {};
+ALIASES['void'] = {};
+ALIASES['volatile_register'] = {};
---
+++ atsamd21g18a/implementors/bare_metal/trait.Nr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["atsamd21g18a"] = [{text:"impl <a class=\"trait\" href=\"bare_metal/trait.Nr.html\" title=\"trait bare_metal::Nr\">Nr</a> for <a class=\"enum\" href=\"atsamd21g18a/enum.Interrupt.html\" title=\"enum atsamd21g18a::Interrupt\">Interrupt</a>",synthetic:false,types:["atsamd21g18a::Interrupt"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

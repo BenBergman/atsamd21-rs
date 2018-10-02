d362b172d6cf1e40cb870e53cf9df2c040ee208f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-02 16:18:05 +0000

    Deploy wez/atsamd21-rs to github.com/wez/atsamd21-rs.git:gh-pages

--- atsamd21e18a/atsamd21_hal/usb/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["UsbDevice",""]],"type":[["DmPad","USB D- is connected here"],["DpPad","USB D+ is connected here"],["SofPad","Emit SOF at 1Khz on this pin when configured as function G"]]});
